d8ee76a88d25ea9ba44944c3b79e3fb8114f24d4
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-09-27 12:13:45 +0200

    bug fix (async function wrapper)

--- index.js
+++ index.js
@@ -28,4 +28,13 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-invokeAction(argv);
+const start = async (argv) => {
+  try {
+    await invokeAction(argv);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start(argv);
+
+// invokeAction(argv);
